86827a2f43e076e913e0067a216ae1166a68149f
zeyyazici <zeyyazman@hotmail.com>
2022-11-27 01:25:25 +0300

    aa
    aa

--- Assets/Teleport.cs
+++ Assets/Teleport.cs
@@ -7,9 +7,8 @@ public class Teleport : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
-        if(other.CompareTag("Player"))
+       
         {
-            Debug.Log("dghgd");
             SceneManager.LoadScene(1);
         }
        
